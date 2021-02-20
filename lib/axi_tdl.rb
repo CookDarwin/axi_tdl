ef53d4af0709b5dfe272477f07e2ed8046bcdcf8
Cook.Darwin <cook_darwin@hotmail.com>
2021-02-21 01:39:18 +0800

    add axi path to tdl_paths

--- lib/axi_tdl.rb
+++ lib/axi_tdl.rb
@@ -5,3 +5,11 @@ module AxiTdl
     AXI_PATH = File.expand_path(File.join(__dir__,"axi"))
     TDL_PATH = File.expand_path(File.join(__dir__,"tdl"))
 end
+
+
+add_to_tdl_paths    File.expand_path(File.join(__dir__, "axi/AXI_stream"))
+add_to_tdl_paths    File.expand_path(File.join(__dir__, "axi/AXI_stream/stream_cache"))
+add_to_tdl_paths    File.expand_path(File.join(__dir__, "axi/AXI4"))
+add_to_tdl_paths    File.expand_path(File.join(__dir__, "axi/AXI4/packet_partition"))
+add_to_tdl_paths    File.expand_path(File.join(__dir__, "axi/common"))
+add_to_tdl_paths    File.expand_path(File.join(__dir__, "axi/data_interface")) 
